175c33f49783559bd99e0985af9b334c4b468ac2
Qiushuo1985 <QiuShuo1985@gmail.com>
2010-08-23 04:01:47 +0000

    disksync: WindowsTrashFileCleaner(cont.)

---
+++ disksync/src/main/java/qiushuo/treasurebox/disksync/WindowsTrashFileCleaner.java
@@ -0,0 +1,143 @@
+package qiushuo.treasurebox.disksync;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * clean windows' invalid file or dir<br/>
+ * (created at 2010-8-23)
+ * 
+ * @author <a href="mailto:QiuShuo1985@gmail.com">QIU Shuo</a>
+ */
+public class WindowsTrashFileCleaner {
+    private static boolean delete = false;
+
+    public static void main(String[] args) throws IOException {
+        if (args == null || args.length < 1) {
+            System.err.println("args[0]: p|d stand for print or delete");
+            System.exit(1);
+        }
+        delete = args[0].equalsIgnoreCase("d");
+        BufferedReader sin = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("clean file/dir under this path:");
+        String rootPath = sin.readLine();
+        File root = new File(rootPath);
+        cleanDir(root);
+    }
+
+    private static void cleanDir(File dir) {
+        File[] fs = dir.listFiles();
+        if (fs == null || fs.length <= 0) return;
+        for (File f : fs) {
+            if (!validName(f)) {
+                deleteFileDir(f);
+            } else if (f.isDirectory()) {
+                cleanDir(f);
+            }
+        }
+    }
+
+    private static void deleteFileDir(File f) {
+        if (!delete) {
+            System.out.println(f.getAbsolutePath());
+        } else {
+            deleteQuietly(f);
+        }
+    }
+
+    private static boolean validName(File f) {
+        String name = f.getName();
+        if (name.contains("/")
+            || name.contains("\\")
+            || name.contains(":")
+            || name.contains("*")
+            || name.contains("?")
+            || name.contains("\"")
+            || name.contains("<")
+            || name.contains(">")
+            || name.contains("|")) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean deleteQuietly(File file) {
+        if (file == null) {
+            return false;
+        }
+        try {
+            if (file.isDirectory()) {
+                cleanDirectory(file);
+            }
+        } catch (Exception e) {
+        }
+
+        try {
+            return file.delete();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private static void cleanDirectory(File directory) throws IOException {
+        if (!directory.exists()) {
+            String message = directory + " does not exist";
+            throw new IllegalArgumentException(message);
+        }
+
+        if (!directory.isDirectory()) {
+            String message = directory + " is not a directory";
+            throw new IllegalArgumentException(message);
+        }
+
+        File[] files = directory.listFiles();
+        if (files == null) { // null if security restricted
+            throw new IOException("Failed to list contents of " + directory);
+        }
+
+        IOException exception = null;
+        for (int i = 0; i < files.length; i++) {
+            File file = files[i];
+            try {
+                forceDelete(file);
+            } catch (IOException ioe) {
+                exception = ioe;
+            }
+        }
+
+        if (null != exception) {
+            throw exception;
+        }
+    }
+
+    private static void deleteDirectory(File directory) throws IOException {
+        if (!directory.exists()) {
+            return;
+        }
+
+        cleanDirectory(directory);
+        if (!directory.delete()) {
+            String message = "Unable to delete directory " + directory + ".";
+            throw new IOException(message);
+        }
+    }
+
+    private static void forceDelete(File file) throws IOException {
+        if (file.isDirectory()) {
+            deleteDirectory(file);
+        } else {
+            boolean filePresent = file.exists();
+            if (!file.delete()) {
+                if (!filePresent) {
+                    throw new FileNotFoundException("File does not exist: " + file);
+                }
+                String message = "Unable to delete file: " + file;
+                throw new IOException(message);
+            }
+        }
+    }
+
+}
